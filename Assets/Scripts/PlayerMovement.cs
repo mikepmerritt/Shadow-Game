f9470d59f492ec68409cdccbbbca3fdd256358ea
mattcmerritt <mattcmerritt@gmail.com>
2021-06-11 15:16:10 -0400

    added placeholder sprites and layers/physics to demo scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private bool OnGround;
+    private float HorizontalMovement;
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
