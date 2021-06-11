89c6593aad609b475ea8e7913dbb4fab2362b4ab
mattcmerritt <mattcmerritt@gmail.com>
2021-06-11 15:33:31 -0400

    added horizontal movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    // Constants
+    private const float Acceleration = 0.2f, MaxSpeed = 5f;
+
+    // State information
     private bool OnGround;
     private float HorizontalMovement;
 
+    // Components
+    public Rigidbody2D rb;
+
     private void Update()
     {
-        
+        // check input
+        HorizontalMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        // apply horizontal acceleration
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + HorizontalMovement * Acceleration, -MaxSpeed, MaxSpeed), rb.velocity.y);
+
+        // stop player if no input
+        if (HorizontalMovement == 0) {
+            rb.velocity = new Vector2(rb.velocity.x / 1.25f, rb.velocity.y);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        OnGround = true;
     }
 }
