cbdf310b237dcc93232b6434ccd9901e1eb07597
mattcmerritt <mattcmerritt@gmail.com>
2021-06-11 15:50:24 -0400

    added simple jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
 
     // Constants
     private const float Acceleration = 0.2f, MaxSpeed = 5f;
+    private const float JumpSpeed = 7f;
 
     // State information
-    private bool OnGround;
+    public bool OnGround;
     private float HorizontalMovement;
+    public bool JumpInput;
 
     // Components
     public Rigidbody2D rb;
@@ -19,21 +21,39 @@ public class PlayerMovement : MonoBehaviour
     {
         // check input
         HorizontalMovement = Input.GetAxisRaw("Horizontal");
+        JumpInput = Input.GetKey(KeyCode.W);
     }
 
     private void FixedUpdate()
     {
+        float xVelocity = rb.velocity.x, yVelocity = rb.velocity.y;
+
         // apply horizontal acceleration
-        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + HorizontalMovement * Acceleration, -MaxSpeed, MaxSpeed), rb.velocity.y);
+        xVelocity = Mathf.Clamp(xVelocity + HorizontalMovement * Acceleration, -MaxSpeed, MaxSpeed);
 
         // stop player if no input
         if (HorizontalMovement == 0) {
-            rb.velocity = new Vector2(rb.velocity.x / 1.25f, rb.velocity.y);
+            xVelocity = xVelocity / 1.25f;
         }
+
+        // jump only if on the ground
+        if (JumpInput && OnGround)
+        {
+            yVelocity = JumpSpeed;
+        }
+
+        rb.velocity = new Vector2(xVelocity, yVelocity);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Debug.Log("Colliding");
         OnGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //Debug.Log("Jumping");
+        OnGround = false;
     }
 }
