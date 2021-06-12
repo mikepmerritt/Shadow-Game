eb355448470946b31b74d41f5e911be2174c8497
mattcmerritt <mattcmerritt@gmail.com>
2021-06-11 21:52:12 -0400

    made shadow and light disappear when player is lost

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
 
 public class InputController : MonoBehaviour
 {
+    // Various component references
     public GameObject Player, Shadow;
     private PlayerMovement PlayerMovement, ShadowMovement;
+    private LightController LightController;
 
+    // Game state
     private float HorizontalMovement;
     private bool JumpInput;
-
-    public Light2D Light;
+    private bool HasActiveShadow;
 
     private void Awake()
     {
         PlayerMovement = Player.GetComponent<PlayerMovement>();
         ShadowMovement = Shadow.GetComponent<PlayerMovement>();
+        LightController = Player.GetComponentInChildren<LightController>();
+
+        HasActiveShadow = true;
     }
 
     private void Update()
@@ -25,13 +29,27 @@ public class InputController : MonoBehaviour
         HorizontalMovement = Input.GetAxisRaw("Horizontal");
         JumpInput = Input.GetButton("Jump");
 
-        // check distance between two
-        Light.pointLightOuterRadius = Mathf.Max(0, 8 - Vector3.Distance(Player.transform.position, Shadow.transform.position));
+        if (HasActiveShadow)
+        {
+            // check distance between two
+            float distance = Vector3.Distance(Player.transform.position, Shadow.transform.position);
+            LightController.UpdateRadius(distance);
+
+            // remove shadow if it is lost
+            if (distance > 5f)
+            {
+                Destroy(Shadow);
+                HasActiveShadow = false;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         PlayerMovement.Move(HorizontalMovement, JumpInput);
-        ShadowMovement.Move(HorizontalMovement, JumpInput);
+        if (HasActiveShadow)
+        {
+            ShadowMovement.Move(HorizontalMovement, JumpInput);
+        }
     }
 }
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightController : MonoBehaviour
+{
+    public Light2D Light;
+    private bool IsFading, LightOut;
+
+    public void Awake()
+    {
+        LightOut = false;
+    }
+
+    public void UpdateRadius(float mangitude)
+    {
+        Light.pointLightOuterRadius = Mathf.Max(8 - mangitude);
+
+        if (Light.pointLightOuterRadius < 3f)
+        {
+            FadeOut();
+        }
+    }
+
+    private void FadeOut()
+    {
+        // kill light over time
+        Debug.Log("Fading to darkness");
+        IsFading = true;
+    }
+
+    private void Update()
+    {
+        if (IsFading)
+        {
+            Light.pointLightOuterRadius = Mathf.Max(0, Light.pointLightOuterRadius - Time.fixedDeltaTime / 2f);
+            if (Light.pointLightOuterRadius <= 0)
+            {
+                LightOut = true;
+            }
+        }
+    }
+}
