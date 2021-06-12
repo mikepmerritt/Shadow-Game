fae6074e1a4b27259abb818e9911d7fca205635f
mattcmerritt <mattcmerritt@gmail.com>
2021-06-12 18:14:48 -0400

    fixed many bugs with respawning, dying, and controllers

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,32 +7,53 @@ public class GameController : MonoBehaviour
 
     public InputController InputController;
     public CameraController CameraController;
+    public LightController LightController;
 
     private GameObject Spawnpoint;
 
     public GameObject PlayerPrefab, ShadowPrefab;
+    public bool IsAlive = true;
 
-    // private void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.R))
-    //     {
-    //         RespawnPlayer();
-    //     }
-    // }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !IsAlive)
+        {
+            RespawnPlayer();
+        }
+    }
 
     public void SetSpawnPoint(GameObject checkpoint)
     {
         InputController.RespawnShadow();
         Spawnpoint = checkpoint;
+  
+        GameObject shadow = GameObject.FindGameObjectWithTag("Shadow");
+        CameraController.SetShadow(shadow);
+        LightController.SetShadow(shadow);
     }
 
     public void RespawnPlayer()
     {
+        CameraController.StopCamera();
+        LightController.Paused = true;
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        Destroy(GameObject.FindGameObjectWithTag("Shadow"));
+        InputController.HasActiveShadow = false;
+
         GameObject player = Instantiate(PlayerPrefab, Spawnpoint.transform.position, Quaternion.identity);
         InputController.ReplacePlayer(player);
         InputController.RespawnShadow();
+        InputController.StartInput();
+
+        GameObject shadow = GameObject.FindGameObjectWithTag("Shadow");
 
         CameraController.SetPlayer(player);
-        CameraController.SetShadow(GameObject.FindGameObjectWithTag("Shadow"));
+        CameraController.SetShadow(shadow);
+        CameraController.StartCamera();
+
+        LightController.ActivateLight();
+        LightController.SetPlayer(player);
+        LightController.SetShadow(shadow);
+        LightController.Paused = false;
     }
 }
