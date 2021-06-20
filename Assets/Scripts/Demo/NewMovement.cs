6a8c3f5988d96e02b65424746379f0592d534956
mattcmerritt <mattcmerritt@gmail.com>
2021-06-20 17:27:58 -0400

    fixed landing and walls, added coyote time, fixed turning

--- Assets/Scripts/Demo/NewMovement.cs
+++ Assets/Scripts/Demo/NewMovement.cs
@@ -6,6 +6,7 @@ public class NewMovement : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    public Collider2D coll;
 
     // horizontal constants
     public float HStartAccel, HStopAccel, HMaxVel;
@@ -23,6 +24,10 @@ public class NewMovement : MonoBehaviour
     public bool OnGround;
     public bool FacingRight = true;
 
+    public bool CoyoteTimeAvailable, CoyoteTimeActive;
+    public float CoyoteTimer;
+    public float CoyoteTimeDuration;
+
     private void Update()
     {
         hIn = Input.GetAxisRaw("Horizontal");
@@ -32,14 +37,23 @@ public class NewMovement : MonoBehaviour
             ApplyJumpForce();
 
         // sprite dir
-        if (FacingRight && rb.velocity.x < 0f || !FacingRight && rb.velocity.x > 0f)
+        if (FacingRight && rb.velocity.x < -0.0001f && hIn < 0f || !FacingRight && rb.velocity.x > 0.0001f && hIn > 0f)
         {
             FacingRight = !FacingRight;
             Vector3 scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
         }
-        
+
+        if (CoyoteTimer <= 0f && CoyoteTimeActive)
+        {
+            CoyoteTimeActive = false;
+            CoyoteTimer = 0f;
+        }
+        else if (CoyoteTimeActive)
+        {
+            CoyoteTimer -= Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
@@ -57,6 +71,8 @@ public class NewMovement : MonoBehaviour
         {
             XVel = 0f;
         }
+
+        OnGround = IsGrounded();
 
         // apply gravity when not grounded
         if (!OnGround)
@@ -78,9 +94,6 @@ public class NewMovement : MonoBehaviour
             }
         }
 
-        // apply jump impulse if jump is input
-
-
         // apply movement to player
         //rb.MovePosition((Vector2) transform.position + new Vector2(XVel * Time.fixedDeltaTime, YVel * Time.fixedDeltaTime));
         rb.velocity = new Vector2(XVel, rb.velocity.y);
@@ -91,32 +104,77 @@ public class NewMovement : MonoBehaviour
         if (OnGround)
         {
             rb.velocity += Vector2.up * JumpStrength;
-            OnGround = false;
+            CoyoteTimeAvailable = false;
+            CoyoteTimeActive = false;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        OnGround = true;
+        if (CheckBelow())
+        {
+            // set a flag for coyote time
+            CoyoteTimeAvailable = true;
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (!OnGround)
+        if (CheckBelow())
         {
-
+            // set a flag for coyote time
+            CoyoteTimeAvailable = true;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        // wait few seconds for coyote time
-        OnGround = false;
+        // wait a second for coyote time
+        if (CoyoteTimeAvailable) 
+        {
+            CoyoteTimer = CoyoteTimeDuration;
+            CoyoteTimeActive = true;
+            CoyoteTimeAvailable = false;
+        }
     }
 
     private bool CheckBelow()
     {
+        float boxHeight = 0.05f;
+        float spacing = 0.05f;
+        Vector2 position = new Vector2(transform.position.x + (FacingRight ? ((BoxCollider2D) coll).offset.x : -((BoxCollider2D) coll).offset.x), transform.position.y + ((BoxCollider2D) coll).offset.y - ((BoxCollider2D) coll).size.y / 2f - boxHeight / 2f);
+        Vector2 size = new Vector2(((BoxCollider2D) coll).size.x - spacing, boxHeight);
+        Collider2D[] detected = Physics2D.OverlapBoxAll(position, size, 0f);
+        
+        foreach (Collider2D collider in detected)
+        {
+            if (collider.tag == "Ground" && collider.gameObject.layer == gameObject.layer)
+            {
+                return true;
+            }
+        }
+
         return false;
+    }
+
+    private bool IsGrounded()
+    {
+        if (!CoyoteTimeActive)
+        {
+            return CheckBelow();
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void OnDrawGizmos() 
+    {
+        float boxHeight = 0.05f;
+        Vector2 position = new Vector2(transform.position.x + (FacingRight ? ((BoxCollider2D) coll).offset.x : -((BoxCollider2D) coll).offset.x), transform.position.y + ((BoxCollider2D) coll).offset.y - ((BoxCollider2D) coll).size.y / 2f - boxHeight / 2f);
+        Vector2 size = new Vector2(((BoxCollider2D) coll).size.x - 0.05f, boxHeight);
+        Gizmos.DrawCube((Vector3) position, (Vector3) size);
     }
 
 }
